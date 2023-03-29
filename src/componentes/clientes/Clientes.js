36923050b5c09c9d2df93ef4dfe35544a3ee7cb3
IgorNSMD <insanmartind@gmail.com>
2023-03-29 14:59:23 -0300

    Capitulo 401

--- src/componentes/clientes/Clientes.js
+++ src/componentes/clientes/Clientes.js
@@ -1,6 +1,21 @@
-import React, { Fragment }  from 'react';
+import React, { useEffect, Fragment }  from 'react';
+
+// importar cliente axios
+import clienteAxios from '../../config/axios';
 
 function Clientes(props) {
+
+    const consultarAPI = async() => {
+        //console.log('consultarAPI...')
+
+        const clientesConsulta = await clienteAxios.get('/clientes');
+        console.log(clientesConsulta)
+    }
+
+    // use effect es similar a componentdidmount y willmount
+    useEffect( () => { 
+        consultarAPI()
+    })
 
     return ( 
         <Fragment>
