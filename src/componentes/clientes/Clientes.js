bdad141ebd1d6dfd078ad85b19e55db589b7ef59
IgorNSMD <insanmartind@gmail.com>
2023-03-29 15:15:26 -0300

    Capitulo 402

--- src/componentes/clientes/Clientes.js
+++ src/componentes/clientes/Clientes.js
@@ -1,15 +1,25 @@
-import React, { useEffect, Fragment }  from 'react';
+import React, { useEffect, useState, Fragment }  from 'react';
 
 // importar cliente axios
 import clienteAxios from '../../config/axios';
 
+
 function Clientes(props) {
+
+    // Trabajar con el state
+    // clientes = state,  guardarClientes = funcion para guardar el state
+    const [ clientes, guardarClientes ] = useState([]);
 
     const consultarAPI = async() => {
         //console.log('consultarAPI...')
 
         const clientesConsulta = await clienteAxios.get('/clientes');
-        console.log(clientesConsulta)
+
+        //console.log(clientesConsulta.data)
+
+        // colocar el resultado en el state
+        guardarClientes(clientesConsulta.data);
+
     }
 
     // use effect es similar a componentdidmount y willmount
@@ -20,6 +30,13 @@ function Clientes(props) {
     return ( 
         <Fragment>
             <h2>Clientes</h2>
+
+            <ul className="listado-clientes">
+                {clientes.map(cliente => {
+                    console.log(cliente)
+                })}
+            </ul>
+
         </Fragment>
     )
 }
