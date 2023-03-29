c4b36239e3dafd1268e084056407d593c9285f86
IgorNSMD <insanmartind@gmail.com>
2023-03-29 11:57:29 -0300

    Capitulo 397

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, {Fragment}  from 'react';
 
 /*** Layout */
 import Header from './componentes/layout/Header';
+import Navegacion from './componentes/layout/Navegacion';
 
 function App() {
   return (
     <Fragment>
        <Header/>
+       <div className="grid contenedor contenido-principal">
+         <Navegacion/>
+
+         <main className="caja-contenido col-9">  
+            { /* TODO: Routing a los diferentes componentes */ }
+         </main>
+
+       </div>
     </Fragment>
 
   );
