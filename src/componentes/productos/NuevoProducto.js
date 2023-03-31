5670ade0199e9f9eeb60d683f125064da0e09e64
IgorNSMD <insanmartind@gmail.com>
2023-03-31 16:22:50 -0300

    Capitulo 421

--- src/componentes/productos/NuevoProducto.js
+++ src/componentes/productos/NuevoProducto.js
@@ -1,36 +1,117 @@
 import React, { useState, Fragment }  from 'react';
+import {useNavigate } from 'react-router-dom';
+
+import Swal from 'sweetalert2';
+
+import clienteAxios from '../../config/axios';
 
 function NuevoProducto(props) {
+    
+    const navigate  = useNavigate();
 
     //producto = state, guardarProducto = setstate
     const [producto, guardarProducto] = useState({
         nombre: '',
         precio: ''
     });
-    
+
     // archivo = state, guardarArchivo = setState
     const [archivo, guardarArchivo] = useState('');
+
+    // almacena el nuevo producto en la base de datos.
+    const agregarProducto = async e => {
+        e.preventDefault();
+
+        // crear un formdata
+        const formData = new FormData();
+        formData.append('nombre', producto.nombre);
+        formData.append('precio', producto.precio);
+        formData.append('imagen', archivo);
+
+        // almacenarlo en la BD
+        try {
+            const res = await clienteAxios.post('/productos', formData, {
+                headers: {
+                    'Content-Type' : 'multipart/form-data'
+                }
+            } );
+
+            // Lanzar una alerta
+            if(res.status === 200) {
+                Swal.fire(
+                    'Agregado Correctamente',
+                    res.data.mensaje,
+                    'success'
+                )
+            }
+
+            // redireccionar
+            navigate(`/productos`);
+
+
+        } catch (error) {
+            console.log(error);
+            // lanzar alerta
+            Swal.fire({
+                type:'error',
+                title: 'Hubo un error',
+                text: 'Vuelva a intentarlo'
+            })
+        }
+    }    
+
+        // leer los datos del formulario
+    const leerInformacionProducto = e => {
+            guardarProducto({
+                // obtener una copia del state y agregar el nuevo
+                ...producto,
+                [e.target.name] : e.target.value
+            })
+        }
+
+    // coloca la imagen en el state
+    const leerArchivo = e => {
+        guardarArchivo( e.target.files[0] );
+    }
 
     return ( 
         <Fragment>
             <h2>NuevoProducto</h2>
 
-            <form>
+            <form
+                onSubmit={agregarProducto}
+            >
                 <legend>Llena todos los campos</legend>
 
                 <div className="campo">
                     <label>Nombre:</label>
-                    <input type="text" placeholder="Nombre Producto" name="nombre" />
+                    <input 
+                        type="text" 
+                        placeholder="Nombre Producto" 
+                        name="nombre"
+                        onChange={leerInformacionProducto}
+                         />
                 </div>
 
                 <div className="campo">
                     <label>Precio:</label>
-                    <input type="number" name="precio" min="0.00" step="0.01" placeholder="Precio" />
+                    <input 
+                        type="number" 
+                        name="precio" 
+                        min="0" 
+                        step="1" 
+                        placeholder="Precio" 
+                        onChange={leerInformacionProducto}
+                    />
                 </div>
             
                 <div className="campo">
                     <label>Imagen:</label>
-                    <input type="file"  name="imagen" />
+                    <input 
+                        type="file"  
+                        name="imagen" 
+                        onChange={leerArchivo}
+                        />
                 </div>
 
                 <div className="enviar">
