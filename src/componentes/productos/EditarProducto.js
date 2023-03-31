92b849d4b555263fbaf3294ddb0b9ba48e15a27f
IgorNSMD <insanmartind@gmail.com>
2023-03-31 16:35:29 -0300

    Capitulo 422

--- src/componentes/productos/EditarProducto.js
+++ src/componentes/productos/EditarProducto.js
@@ -1,6 +1,36 @@
-import React, { Fragment }  from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
+import {useNavigate, useParams   } from 'react-router-dom';
+
+import Swal from 'sweetalert2';
+
+import clienteAxios from '../../config/axios';
+import Spinner from '../layout/Spinner';
 
 function EditarProductos(props) {
+
+    // obtener el ID
+    const { id } = useParams();
+
+    // producto = state, y funcion para actualizar
+    const [ producto, guardarProducto ] = useState({
+        nombre: '',
+        precio: '',
+        imagen : ''
+    });    
+
+
+    
+    // cuando el componente carga
+    useEffect(() => { 
+        
+        // consultar la api para traer el producto a editar
+        const consultarAPI = async () => {
+            const productoConsulta = await clienteAxios.get(`/productos/${id}`);
+            guardarProducto(productoConsulta.data);
+        }    
+
+        consultarAPI();
+    }, [])
 
     return ( 
         <Fragment>
